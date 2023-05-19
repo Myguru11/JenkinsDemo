fefefd5dee03c27e4d446b30bf690afdbcda993c
Myguru11 <133929669+Myguru11@users.noreply.github.com>
2023-05-18 19:08:46 -0700

    Update AppTest.java

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void test1()
+    {
+        System.out.println("Testcase1 completed");
+    }
+    @Test
+    public void test2()
+    {
+        System.out.println("Testcase2 completed");
+    }
+    @Test
+    public void test3()
+    {
+        System.out.println("Testcase3 completed");
+    }
+}
